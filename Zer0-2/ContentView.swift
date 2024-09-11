4661013ba8f8cdfed71cd7e5503d2a04d837f32c
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-09-11 20:28:39 +0100

    Initial Commit

---
+++ Zer0-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zer0-2
+//
+//  Created by Eugen Ionita on 11/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zer0-2/Zer0_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Zer0_2App.swift
+//  Zer0-2
+//
+//  Created by Eugen Ionita on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Zer0_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
